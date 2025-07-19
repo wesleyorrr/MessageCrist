ea1623ccb7a04cfde1c9e98515eca5695756ef25
Wesley de Oliveira <wesleysistemaor@gmail.com>
2025-07-19 20:38:04 -0300

    -Adicionando pacotes para organizanção e implementação do mvvm -criação da class SplashActivity para fazer Splashscreen da aplicação -refatoramos a mainActivity para o pacote de ui de interface

--- app/src/main/java/com/example/mymensagecrist/ui/MainActivity.kt
+++ app/src/main/java/com/example/mymensagecrist/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.mymensagecrist
+package com.example.mymensagecrist.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mymensagecrist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
